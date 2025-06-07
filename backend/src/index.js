90e75d4a6c605d6a5d70870be4afcaafa26800ff
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-06-08 00:28:52 +0530

    Basic Backend Setup updated with setup of middlewares.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import { PORT } from "./config/serverConfig.js";
 
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    return res.json({
+"message": "Welcome to the backend server",
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
